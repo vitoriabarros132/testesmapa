1235704df9abc1f06634ca648a331b168348dfb6
Vitória Barros <108809563+vitoriabarros132@users.noreply.github.com>
2025-05-28 11:13:28 -0300

    Update script.js
    teste de tile layer

--- script.js
+++ script.js
@@ -32,9 +32,9 @@ document.addEventListener('DOMContentLoaded', () => {
       //  attribution: '© OpenStreetMap'
   //  }).addTo(mapa);
 
-    L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
-    maxZoom: 19, // Stamen Toner geralmente vai até 18 ou 19
-    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+    maxZoom: 19 // Geralmente vai até zoom 19
     }).addTo(mapa);
     
     //-------------------------------------------------------------------
